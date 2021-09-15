c1d2e5c6bb6477066baed43d0ca48e2d9a86b2c2
Timothy James <41609671+timothyrjames@users.noreply.github.com>
2021-09-15 19:30:05 -0400

    Create main.py

---
+++ step02/gae-project/main.py
@@ -0,0 +1,11 @@
+import flask
+
+
+app = flask.Flask(__name__)
+
+@app.route('/')
+def root():
+    return flask.redirect("/s/index.html", code=302)
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
