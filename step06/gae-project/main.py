abc289cd0c1c165fa31764000847378df6bf246f
timothyrjames@yahoo.com <timothyrjames@yahoo.com>
2021-09-20 22:28:26 +0000

    Adding second templates example

---
+++ step06/gae-project/main.py
@@ -0,0 +1,44 @@
+import flask
+
+app = flask.Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index.html')
+def root():
+    return flask.render_template('index.html', page_title='Main Page')
+
+
+@app.route('/page1.html')
+def first_page():
+    return flask.render_template('page1.html',
+                                 page_title='Page 1',
+                                 error_message='There was some error')
+
+
+@app.route('/page2.html')
+def second_page():
+    fruit_list = [
+        'Apples',
+        'Bananas',
+        'Cherries',
+        'Dates',
+    ]
+    return flask.render_template('page2.html',
+                                 page_title='Page 2', fruits=fruit_list)
+
+
+@app.route('/page3.html')
+def third_page():
+    states_dict = {
+        'PA': 'Pennsylvania',
+        'OH': 'Ohio',
+        'WV': 'West Virginia',
+    }
+    return flask.render_template('page3.html',
+                                 page_title='Third Page',
+                                 states=states_dict)
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
